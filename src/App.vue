40a649f6709e034201cab059bd00140c3883ad43
junaidjahan <junaidjahan50@gmail.com>
2025-12-21 18:24:00 +0500

    feat:✨sidebar design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+import { useSidebar } from '@/composables/useSidebar'
+
+const { isCollapsed } = useSidebar()
+
+const mainMargin = computed(() => {
+  return isCollapsed.value ? 'ml-[72px]' : 'ml-[190px]'
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex min-h-screen bg-[var(--color-base-bg)]">
+    <Sidebar />
+    <main
+      class="flex-1 transition-all duration-300"
+      :class="mainMargin"
+    >
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
