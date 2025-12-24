6fc1026db60f81f42de48eb7a1b4462e3b0af8cc
junaidjahan <junaidjahan50@gmail.com>
2025-12-24 12:05:35 +0500

    fix:💄backdrop color

--- src/views/my-payslip/components/SalaryDrawer.vue
+++ src/views/my-payslip/components/SalaryDrawer.vue
@@ -1,10 +1,9 @@
 <template>
   <Drawer direction="bottom" :open="open" @update:open="$emit('update:open', $event)">
-    <DrawerContent class="min-h-[94vh]! rounded-t-4xl! focus:outline-none sm:max-w-[94%] mx-auto">
+    <DrawerContent class="min-h-[94vh]! rounded-t-4xl! focus:outline-none sm:max-w-[94%] mx-auto"
+      overlayClass="bg-base-secondary-950/77">
       <!-- Header -->
-      <div
-        class="flex items-center justify-between px-14 py-8 border-b border-b-base-secondary-950"
-      >
+      <div class="flex items-center justify-between px-14 py-8 border-b border-b-base-secondary-950">
         <div class="flex items-center gap-4">
           <!-- Company Name -->
           <span class="text-base-secondary-950 text-sm">Mercans USA Ltd.</span>
@@ -24,12 +23,8 @@
         <!-- Close Button -->
         <DrawerClose as-child>
           <button
-            class="size-12 flex items-center justify-center bg-base-secondary-850 text-white rounded-2xl group cursor-pointer transition-all duration-200 hover:bg-base-secondary-850"
-          >
-            <Icon
-              name="close"
-              class="size-9 group-hover:rotate-180 transition-transform duration-400"
-            />
+            class="size-12 flex items-center justify-center bg-base-secondary-850 text-white rounded-2xl group cursor-pointer transition-all duration-200 hover:bg-base-secondary-850">
+            <Icon name="close" class="size-9 group-hover:rotate-180 transition-transform duration-400" />
           </button>
         </DrawerClose>
       </div>
@@ -37,8 +32,7 @@
       <!-- Content -->
       <div class="p-6 h-full">
         <div
-          class="border border-dashed border-gray-200 rounded-2xl h-full min-h-[70vh] p-6 flex items-center justify-center"
-        >
+          class="border border-dashed border-gray-200 rounded-2xl h-full min-h-[70vh] p-6 flex items-center justify-center">
           <!-- Empty State -->
           <EmptyContent alt="No designs" :containerClass="''" imageClass="mb-4" />
         </div>
